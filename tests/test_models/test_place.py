d2ab65a8f59c0d15684ce72c9d2a2e6a20ee8e95
der-bew <derbew12@gmail.com>
2022-01-27 14:05:00 +0300

    Add some test case

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Contains the TestPlaceDocs classes
-"""
+""" A Unit test module for place class """
 
 from datetime import datetime
 import inspect
@@ -13,14 +11,14 @@ Place = place.Place
 
 
 class TestPlaceDocs(unittest.TestCase):
-    """Tests to check the documentation and style of Place class"""
+    """ Tests to check the documentation and style of Place class """
     @classmethod
     def setUpClass(cls):
         """Set up for the doc tests"""
         cls.place_f = inspect.getmembers(Place, inspect.isfunction)
 
     def test_pep8_conformance_place(self):
-        """Test that models/place.py conforms to PEP8."""
+        """ Test that models/place.py conforms to PEP8. """
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['models/place.py'])
         self.assertEqual(result.total_errors, 0,
